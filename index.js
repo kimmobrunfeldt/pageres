1fe041ff9c9d985bb72083f122a0e05b4638f25a
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-02-20 21:34:57 +0200

    Add option to speficy savepath

--- index.js
+++ index.js
@@ -3,39 +3,42 @@ var webshot = require('webshot');
 var eachAsync = require('each-async');
 var slugifyUrl = require('slugify-url');
 
-function generateSizes(url, sizes, cb) {
-	eachAsync(sizes, function (el, i, next) {
-		// strip `www.` and convert to valid filename
-		var filenameUrl = slugifyUrl(url.replace(/^(?:https?:\/\/)?www\./, ''));
-		var filename = filenameUrl + '-' + el + '.png';
-		var dim = el.split(/x/i);
-		var options = {
-			windowSize: {
-				width: dim[0],
-				height: dim[1]
-			},
-			shotSize: {
-				width: 'window',
-				height: 'all'
-			}
-		};
+function generateSizes(shot, sizes, cb) {
+    eachAsync(sizes, function(el, i, next) {
+        // strip `www.` and convert to valid filename
+        var filenameUrl = slugifyUrl(shot.url.replace(/^(?:https?:\/\/)?www\./, ''));
+        filenameUrl += '-' + el;
+        var baseName = shot.filePath || filenameUrl;
+        var filename = baseName + '.png';
 
-		webshot(url.toLowerCase(), filename, options, next);
-	}, cb);
+        var dim = el.split(/x/i);
+        var options = {
+            windowSize: {
+                width: dim[0],
+                height: dim[1]
+            },
+            shotSize: {
+                width: 'window',
+                height: 'all'
+            }
+        };
+
+        webshot(shot.url.toLowerCase(), filename, options, next);
+    }, cb);
 }
 
-module.exports = function (urls, sizes, cb) {
-	cb = cb || function () {};
+module.exports = function(shots, sizes, cb) {
+    cb = cb || function() {};
 
-	if (urls.length === 0) {
-		return cb(new Error('`urls` required'));
-	}
+    if (shots.length === 0) {
+        return cb(new Error('`shots` required'));
+    }
 
-	if (sizes.length === 0) {
-		return cb(new Error('`sizes` required'));
-	}
+    if (sizes.length === 0) {
+        return cb(new Error('`sizes` required'));
+    }
 
-	eachAsync(urls, function (url, i, next) {
-		generateSizes(url, sizes, next);
-	}, cb);
+    eachAsync(shots, function(url, i, next) {
+        generateSizes(url, sizes, next);
+    }, cb);
 };
